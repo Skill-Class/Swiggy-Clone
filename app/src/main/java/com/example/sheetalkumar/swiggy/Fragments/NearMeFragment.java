94f5c86cc9eba956d042ff3be8094e0494815e22
sheetalkumar <sheetal.mca17.du@gmail.com>
2018-11-04 00:07:20 +0530

    RecyclerView onTouchListener added.

--- app/src/main/java/com/example/sheetalkumar/swiggy/Fragments/NearMeFragment.java
+++ app/src/main/java/com/example/sheetalkumar/swiggy/Fragments/NearMeFragment.java
@@ -1,6 +1,7 @@
 package com.example.sheetalkumar.swiggy.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,9 +9,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.sheetalkumar.swiggy.Adapters.ItemAdapter;
 import com.example.sheetalkumar.swiggy.Adapters.OfferAdapter;
@@ -52,7 +56,43 @@ public class NearMeFragment extends Fragment {
         ItemAdapter adapterforItem = new ItemAdapter(getContext(), itemImages,itemName);
         itemRecyclerView.setAdapter(adapterforItem);
 
+//        addRecyclerTouchListner(offterRecyclerView);
+
+
+       offterRecyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity(),
+                offterRecyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, final int position) {
+                Toast.makeText(getActivity(), "Showing Position  (Single Press) : " + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+                Toast.makeText(getActivity(), "Showing Position  (Long Press) : " + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        }));
+
+        itemRecyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity(),
+                itemRecyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, final int position) {
+                Toast.makeText(getActivity(), "Showing Position  (Single Press) : " + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+                Toast.makeText(getActivity(), "Showing Position  (Long Press) : " + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        }));
         return rootView;
+
+    }
+
+    private void addRecyclerTouchListner(RecyclerView recyclerView) {
 
     }
 
@@ -92,5 +132,62 @@ public class NearMeFragment extends Fragment {
         itemName.add("34 Chawringhee Lane");
 
     }
+    //RECYCLER VIEW ANIMATION ENDS
+
+
+    //RECYCLER VIEW ONCLICK METHOND STARTS
+    public static interface ClickListener {
+        public void onClick(View view, int position);
+
+        public void onLongClick(View view, int position);
+    }
+
+    class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+        private NearMeFragment.ClickListener clicklistener;
+        private GestureDetector gestureDetector;
+
+        public RecyclerTouchListener(Context context, final RecyclerView recycleView,
+                                     final NearMeFragment.ClickListener clicklistener) {
+
+            this.clicklistener = clicklistener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recycleView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clicklistener != null) {
+                        clicklistener.onLongClick(child, recycleView.getChildAdapterPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && clicklistener != null && gestureDetector.onTouchEvent(e)) {
+                clicklistener.onClick(child, rv.getChildAdapterPosition(child));
+            }
+
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+    }
+
 
 }
+
